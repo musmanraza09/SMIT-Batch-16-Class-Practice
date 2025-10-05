667b67f93615984e258a3150537586b7b998819f
musmanraza09 <musmanraza09@gmail.com>
2025-10-05 09:54:22 +0500

    Object Topic is start

---
+++ Class 02/app.js
@@ -0,0 +1,77 @@
+console.log("JavaScript Connected!");
+//<<<<<<<<<<<<<<<<<Chap:63 >>>>>>>>>>>>//
+//<<<<<<<<<<<<<<<<Getting a target's name >>>>>>>>>>>///
+
+// var parent = document.getElementById("div1");
+
+// var child = parent.firstElementChild;
+// var nodename = child.nodename;
+// console.log(nodename);
+
+// var parent = document.getElementById("div1");
+// var child = parent.firstchild;
+// console.log(child.nodeName);
+// console.log(child.nodeValue);
+
+
+// var para = document.getElementById("para");
+
+// var first = para.firstChild;
+
+// if(first.nodeName === "#text"){
+//     console.log ("This is text node :" + first.nodeValue);
+// }
+
+
+
+
+// var target = document.getElementById("p1");
+
+// var specialclass = target.hasAttribute("class");
+
+// console.log(specialclass);
+
+
+
+// var target = document.getElementById("div1");
+
+// var atrrval = target.getAttribute("class");
+// console.log(atrrval);
+
+// var target = document.getElementById("div3");
+
+// target.setAttribute("class" , "highlight");
+
+// console.log(target)
+
+
+// var target = document.getElementById("div4");
+
+// target.setAttribute("class" , "blue");
+
+// console.log(target.getAttribute("class"));
+
+// var list = document.getElementById("p1").attributes;
+
+// console.log(list.length);
+
+// var list = document.getElementById("p1").attributes;
+// console.log(list[1].nodeName);
+// console.log(list[1].nodeValue);
+
+// var list = document.getElementById("p1").attributes;
+
+// for(var i = 0; i < list.length; i++){
+//     console.log("Name: " , list[i].nodeName, "Value: " , list[i].nodeValue);
+// }
+
+// var list = document.getElementById("p1");
+
+// list.className = "c1 red highlight";
+
+
+//to do list
+
+
+
+
---
+++ Class 03/app.js
@@ -0,0 +1,116 @@
+// console.log("JavaScript connected");
+
+// let plan1Name = "basic";
+// let plan1price = 3.99;
+// let plan1space = 100;
+// let plan1Date = 1000;
+// let plan1Page = 10;
+
+
+// let plan2Name = "Prof";
+// let plan2price = 5.99;
+// let plan2space = 500;
+// let plan2Date = 5000;
+// let plan2Page = 50;
+
+
+
+// let plan3Name = "ultimate";
+// let plan3price = 9.99;
+// let plan3space = 200;
+// let plan3Date = 2000;
+// let plan3Page = 500;
+
+//objects
+
+// let plan1 = {
+//     name:"Basic",
+//     price:3.99,
+//     space: 100,
+//     data: 1000,
+//     page: 10
+// }
+
+// let plan2 = {
+//     name:"Prof",
+//     price:5.99,
+//     space: 500,
+//     data: 5000,
+//     page: 50
+// }
+
+// //plan1
+// plan1.discountMonths = [6,7,11];
+
+// console.log(plan1)
+
+// //plan2
+// plan2.discountMonths = [6,7,11];
+
+// console.log(plan2)
+
+
+// //dot notation
+// //plan1
+// console.log(plan1.name);
+
+// console.log(plan1.data);
+
+// console.log(plan1.price);
+
+// console.log(plan1.space);
+
+// console.log(plan1.page);
+
+// //plan2
+
+
+// console.log(plan2.name);
+
+// console.log(plan2.data);
+
+// console.log(plan2.price);
+
+// console.log(plan2.space);
+
+// console.log(plan2.page);
+
+
+// let plan3 = {
+//     name: "Ultimate",
+//     price: 9.99,
+//     space: 2000,
+//     data: 20000,
+//     page: 500
+// }
+
+// plan3.discountMonths = [7,6,11];
+// console.log(plan3);
+
+// let month = plan3.discountMonths[2];
+// console.log(month);
+// console.log(plan3.data,plan3.name,plan3.page,plan3.price,plan3.space);
+
+
+// let car = {
+//     name:"Honda",
+//     model:"Civic",
+//     year:"2025",
+//     color:"black"
+// }
+
+// let userPrompt = prompt("Type your name");
+
+
+// let std = {
+//     name:userPrompt,
+// }
+// .console.log(std);
+
+// let std = {
+//     names:["Meer" , "Hafeez" , "Ali" , "Usman" ],
+//     obj : {
+//         name:"Meer Quais",
+//     },
+// }
+// console.log(std.obj)
