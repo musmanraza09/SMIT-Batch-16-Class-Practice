be984cb399f2c4fa95f835f0064efee49cca8f99
musmanraza09 <musmanraza09@gmail.com>
2025-11-08 11:26:55 +0500

    Book is Done!

--- Class 07/app.js
+++ Class 07/app.js
@@ -1,4 +1,4 @@
-//console.log("JavaScript Connected!");
+console.log("JavaScript Connected!");
 
 
 // var whereWeAt = window.location.href;
@@ -9,58 +9,77 @@
 
 // console.log(theDomain);
 
-
 // var thePath = window.location.pathname;
 
 // console.log(thePath);
 
+// window.location.href = "https://www.google.com/"
 
-
-//   window.location.href = "https://www.google.com/"
 
 
 // window.location.assign("https://www.google.com");
 
 
-//window.location.replace("https://youtube.com");
-
+// window.location.replace("https://www.youtube.com/");
 
 // window.location.reload();
 
 
-// window.location.href = window.location.href;
+// window.location.href = window.location.href 
 
-// var path = document.URL;
+
+// var path = document.URL
 // console.log(path);
 
+
+
 // function backFunc(){
-//     // history.back;
-//     history.go(-1);
+//   // history.back();
+//   history.go(-1)
 // }
 
-// function goforward(){
-// // history.forward();
-// history.go(1);
+// function goFor(){
+//   // history.forward()
+//   history.go(1)
 // }
 
 // var whereUserCameFrom = document.referrer;
 
 // console.log(whereUserCameFrom);
 
-// var monkeyWindow = window.open("about.html");
+// var monkeyWindow = window.open();
 
 // var windowContent = `
-// <h1>Capuchin monkey</h1>
-// <img src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5cr1gu9iW2KUP2mtFtzMWSIpQ49IT-IdmulVpbqFqS9ES83owFMGaUcT8LE6Z283UupyTdmPS0xPY9jWf5tvDwsEdBrpJdXhjJK5D20G9&s=10' >
-
-// <p>    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum quidem incidunt at quos minima commodi nihil magnam, doloribus fugiat ipsam culpa maiores modi, hic, suscipit vel repudiandae laborum unde eos.</p>
-
-
+//   <h1>Capuchin monkey</h1>
+//   <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFfcog-vgY9cLOMB-YeN-9ffJaM5_4SJ4_yzc0V6jk4bS6VD43ngpNzJDwsmM9bHQFWquP55nWxsNaAdIsLcRRvH4urjOK4GGzSrtPNw&s=10'>
+//   <p>The word capuchin comes from a group of fraiars called the order
+//   of Friars Minor Capuchin, who wear brown robes with big hoods covering their heads.</>
 // `;
 
 // monkeyWindow.document.write(windowContent);
 
-// monkeyWindow.close();
+// monkeyWindow.close()
 
 
-//let monkeyWindow = window.open("monk.html" , "win1");
+// var monkeyWindow = window.open("about.html" , "win1");
+
+
+
+// var monkeyWindow = window.open("monkey.html","win1");
+
+// var monkeyWindow = window.open("" , "win1");
+// var windowContent = `
+//   <h1>Capuchin monkey</h1>
+//   <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFfcog-vgY9cLOMB-YeN-9ffJaM5_4SJ4_yzc0V6jk4bS6VD43ngpNzJDwsmM9bHQFWquP55nWxsNaAdIsLcRRvH4urjOK4GGzSrtPNw&s=10'>
+//   <p>The word capuchin comes from a group of fraiars called the order
+//   of Friars Minor Capuchin, who wear brown robes with big hoods covering their heads.</>
+// `;
+
+// monkeyWindow.document.write(windowContent);
+
+
+// var monkeyWindow = window.open("monk.html" , "win1" , "width=420 , height=380 , left=200 , top=100");
+
+// window.open(" " , "pop1");
+
+var monkeyWindow = window.open("monkey.html","win1");
--- Class 08/app.js
+++ Class 08/app.js
@@ -1,68 +1,289 @@
-console.log("JavaScript Connected!");
+console.log("Js Connected!");
 
-//  function checkForSelection() {
-//    if (document.getElementById("states").selectedIndex === 0) {
-//       alert("Please select a state.");
-//       return false;
-//     }
-// //   }
+// function checkForSelection(){
 
-//  form validation for select box
-//  function checkForSelection(selecID) {
-//    var target = document.getElementById(selecID);
-//     if (target.selectedIndex === 0) {
-//       alert("Please select a state.");
-//       return false;
-//     }
-// return false;
-
-
+//   if(document.getElementById("states").selectedIndex === 0){
+//     alert("please select a state.")
+//     return false;
 //   }
 
-//   function validateRadios() {  
-// var radios = document.getElementsByName("r1") ;   
-//  }
-//  for (var i = 0; i < radios.length; i++) {
-//  if (radios[i].checked) {
-//  return true; 
-// }
-//  }
-//  alert("Please check one.");
-//  return false;
-// // }
-// function validateZip(){
-//     var InpValue = document.getElementById("zip").value.trim();
 
-//     if(InpValue.length !== 5){
-//         alert
-
-//     }
 // }
 
 
-// function validateZip(){
-//     var v = document.getElementById("zip").ariaValueMax.trim();
-//     var regex = !/^\d{5}$/;
+// function checkForSelection(selectID){
+
+//   var target = document.getElementById(selectID);
+
+//   if(target.selectedIndex === 0){
+//     alert("Please select a state.");
+//     console.log(target.selectedIndex);
     
-//     if(!regex.test(v)){
-//         alert("Please enter exactly 5 digits")
-//         return false;
-//     }
-//     return true;
+//     target.focus();
+//     return false
+//   }
+
+//   return true
+
+
 // }
 
-// console.log(validatePassword("usman1@"))
 
-function validateEmail(){
-    var email = document.getElementById("email").value.trim();
+// function validateRadios(){
+//   var radios = document.getElementsByName("r1");
 
-    var at = email.indexof("@");
+//   for(var i = 0;i < radios.length;i++){
+//     if(radios[i].checked){
+//       return true
+//     }
+//   }
+//   alert("Please check one.");
+//   return false;
 
-    if(at < 1 ||at > email.length - 5){
-        alert("Invalid Position of @");
-        return false;
-    }
-return true;
+
+// }
+
+// function validateZip(){
+
+//   var InpValue = document.getElementById("zip").value.trim();
+
+//   if(InpValue.length < 5){
+//     alert("Please enter a 5-digit code.")
+//     return false;
+//   }
+//   return true;
+// }
+
+// function validateZip(){
+
+//   var InpValue = document.getElementById("zip").value.trim();
+
+//   if(InpValue.length !== 5){
+//     alert("Enter 5 digits!");
+//     return false;
+//   }
+
+//   for(var i=0; i < 5; i++){
+//     if(isNaN(Number(InpValue[i]))){
+//       alert("Only digits Allowed");
+//       return false;
+//     }
+//   }
+
+//   return true;  
+// }
+
+
+// function validateZip(){
+//   var v = document.getElementById("zip").value.trim();
+//   var regex = /^\d{5}$/
+//   if(!regex.test(v)){
+//     alert("Please enter exactly 5 digits!")
+//     return false;
+//   }
+//   return true;
+// }
+
+
+// function validatePassword(password) {
+//   const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/;
+//   return passwordRegex.test(password);
+// }
+
+// console.log(validatePassword("meer1@SS"));
+
+
+// check empty string
+
+// function validateEmail(){
+//   var email = document.getElementById("email").value.trim();
+
+//   if(email === ""){
+//     alert("please fill the email!");
+//     return false;
+//   }
+
+//   return true;
+// }
+
+
+// check poisiton of @
+
+
+// function validateEmail(){
+//   var email = document.getElementById("email").value.trim();
+
+//     var at = email.indexOf("@");
+
+//     if(at < 1 || at > email.length - 5){
+//       alert("Invalid Poistion of @");
+//       return false;
+//     }
+
+//     return true;
+
+
+// }
+
+ // check Dot (.) after @
+
+// function validateEmail(){
+//   var email = document.getElementById("email").value.trim();
+
+//     var at = email.indexOf("@");
+//     var dot = email.indexOf(".");
+
+//     if(dot - at < 2 || dot > email.length - 3){
+//       alert("Dot Location is Invalid!");
+//       return false;
+//     }
+//    return true;
+
+
+// }
+
+// function validateEmail(){
+
+// var email = document.getElementById("email").value.trim();
+
+
+// var firstAt = email.indexOf("@");
+// var lastAt = email.lastIndexOf("@");
+
+
+
+// if(firstAt === -1 || firstAt !== lastAt){
+//   alert("Email must contain exactly one @!");
+//   return false;
+// }
+
+// return true;
+
+// }
+
+
+// function validateEmail(){
+//   var email = document.getElementById("email").value.trim();
+
+
+//   var regex = /^\S+@\S+\.\S{2,4}$/;
+
+//   if(!regex.test(email)){
+//     alert("Please enter a valid email");
+//     return false;
+//   }
+
+//   return true;
+
+
+// }
+
+
+// function validateEmail(){
+//   var email = document.getElementById("email").value.trim();
+
+
+//   var regex =  /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+//   if(!regex.test(email)){
+//     alert("Please enter a valid email");
+//     return false;
+//   }
+
+//   return true;
+
+
+// }
+
+// function greetWorld(){
+//   var greeting = "Hello World!";
+//   aler(greeting);
+// }
+
+// greetWorld()
+
+
+// try{
+//   // code jo run karna hai
+// }
+// catch(error){
+//   // error aane par ye chalega
+// }
+
+// function greetWorld(){
+
+//   try{
+//     var greeting = "Hello World!";
+//     aler(greeting);
+//   }
+//   catch(err){
+//     alert("Error mila: " + err)
+//   }
+
+
+// }
+
+
+// try{
+//   console.log("Starting code...");
+
+//   let x = 10;
+//   let y = z + 5;
+
+//   console.log("This line will not run!"); 
+// }
+// catch(error){
+//   console.log("Error aaya: " + error);
+  
+// }
+
+// try{
+//   let num = 10;
+//   console.log(num / 0); // no error
+// }
+// catch(error){
+//   console.log("Error: " + error);
+// }
+// finally{
+//   console.log("Finally Block chala!");
+  
+// }
+
+
+// try{
+//   console.log(a + 2); // a define nahi hai // error
+// }
+// catch(e){
+//   console.log("Error Type: " + e.name);
+//   console.log("Error Message: " + e.message);
+  
+// }
+
+
+// function divideNumbers(a, b){
+//   try {
+//     if(isNaN(a) || isNaN(b)){
+//       throw "please enter only numbers"
+//     }
+//     console.log(a / b);
+    
+//   } catch (error) {
+//     console.log("Error: " + error);
+    
+//   }
+// }
+
+// divideNumbers(10, "abc");
+
+
+try {
+  var age = 121;
+  if(age < 18){
+    throw "Underage"
+  }
+  console.log("allowed!");
+  
+} catch (error) {
+  console.log("Error: " + error);
+  
 }
-
-
